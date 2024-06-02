d3a7b0d58b179cd1daae7c3fc63effe71c0839b4
Paulo-Mikhael <paulomiguel11111971@gmail.com>
2024-06-02 18:06:54 -0400

    Tirando imports desnecessários

--- src/Pages/MainScreen/index.tsx
+++ src/Pages/MainScreen/index.tsx
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 import ContactButton from "../../Components/ContactButton";
-import Header from "../../Components/Header";
 
 const MainBackground = styled.main`
   width: 100%;
